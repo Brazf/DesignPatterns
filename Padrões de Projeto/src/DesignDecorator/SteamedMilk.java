32e11db33e7788ef0868bb6bb9b244f477462d8f
Fábio Braz <fabiobrazferreira@gmail.com>
2023-03-09 16:11:59 -0300

    update

---
+++ Padrões de Projeto/src/DesignDecorator/SteamedMilk.java
@@ -0,0 +1,23 @@
+package DesignDecorator;
+
+public class SteamedMilk extends CondimentDecorator{
+	
+	public SteamedMilk (Beverage beverage) {
+		this.beverage = beverage;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return beverage.getDescription() + "Steamed Milk";
+	}
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return beverage.cost()	+ 0.10;
+	}
+	
+	
+	
+}
