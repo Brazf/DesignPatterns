bdb669b52a966fa762c525d709b1331077f53486
Fábio Braz <fabiobrazferreira@gmail.com>
2023-03-19 16:29:56 -0300

    update

---
+++ Padrões de Projeto/src/DesignFactory/PizzaTestDrive.java
@@ -0,0 +1,17 @@
+package DesignFactory;
+
+public class PizzaTestDrive {
+
+	public static void main(String[] args) {
+		
+		PizzaStore nyStore = new NYPizzaStore();
+		PizzaStore ChicagoStore = new ChicagoPizzaStore();
+		
+		Pizza pizza = nyStore.orderPizza("cheese");
+		System.out.println("Ethan ordered a "+ pizza.getName()+"\n");
+		
+		pizza = ChicagoStore.orderPizza("Cheese");
+		System.out.println("Joel ordered a "+ pizza.getName()+"\n");
+		
+	}
+}
