3a870d59c4ae62dec6047fb1216651a5be1db520
Fábio Braz <fabiobrazferreira@gmail.com>
2023-03-26 07:20:30 -0300

    update

---
+++ Padrões de Projeto/src/DesignIterator/PancakeMenuIterator.java
@@ -0,0 +1,30 @@
+package DesignIterator;
+
+import java.util.List;
+
+public class PancakeMenuIterator implements Iterator{
+
+	List<MenuItem> items;
+	int position =0;
+
+	public PancakeMenuIterator(List<MenuItem> items) {
+		this.items = items;
+	}
+	
+	public MenuItem next () {
+		MenuItem menuItem = items.get(position);
+		position = position +1;
+		return menuItem;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		if(position >= items.size() || items.get(position) == null ) {
+			return false;	
+		}
+		else {
+			return true;
+		}
+	}
+	
+}
