9ea5f4a61377d27ac79dbe04271e3ba499f234e2
Fábio Braz <fabiobrazferreira@gmail.com>
2023-03-29 22:27:50 -0300

    update

---
+++ Padrões de Projeto/src/DesignFactory/ChicagoPizzaStore.java
@@ -0,0 +1,11 @@
+package DesignFactory;
+
+public class ChicagoPizzaStore extends PizzaStore {
+
+	@Override
+	protected Pizza createPizza(String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
